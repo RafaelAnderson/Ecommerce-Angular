dc4b2d8ad07bfd16aa5f0fb21b3cc313d9d86a36
RafaelAnderson <rp_9966@hotmail.com>
2021-12-28 22:17:40 -0500

    36 y 37 - configuracion Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,21 @@
 export const environment = {
   production: true,
-  name: 'prod'
+  name: 'prod',
+  firebase: {
+    config: {
+
+      apiKey: "AIzaSyBo2pJR_uKJpK9Svb8lnJEpaFbbx0X5PNc",
+
+      authDomain: "ecommerce-400e8.firebaseapp.com",
+
+      projectId: "ecommerce-400e8",
+
+      storageBucket: "ecommerce-400e8.appspot.com",
+
+      messagingSenderId: "489499298233",
+
+      appId: "1:489499298233:web:b2ed01844bd9b7c29f8e68"
+
+    }
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,25 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  name: 'default',
+  firebase: {
+    config: {
+
+      apiKey: "AIzaSyBo2pJR_uKJpK9Svb8lnJEpaFbbx0X5PNc",
+
+      authDomain: "ecommerce-400e8.firebaseapp.com",
+
+      projectId: "ecommerce-400e8",
+
+      storageBucket: "ecommerce-400e8.appspot.com",
+
+      messagingSenderId: "489499298233",
+
+      appId: "1:489499298233:web:b2ed01844bd9b7c29f8e68"
+
+    }
+  }
 };
 
 /*
